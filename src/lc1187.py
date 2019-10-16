57ed78e25589beccef10155e1639e844f296a001
zxiang77 <zxiang77@yahoo.com>
2019-10-15 20:48:50 -0400

    lc1187

--- src/lc1187.py
+++ src/lc1187.py
@@ -1,35 +1,22 @@
+import bisect
+import collections
 from typing import List
 
 
 class Solution:
     def makeArrayIncreasing(self, arr1: List[int], arr2: List[int]) -> int:
         arr2.sort()
-
-        minctr = float('inf')
-
-        prevSorted = True
-        arr1.insert(0, float('-inf'))
-        s1, s2 = 0, 0
-        for i in range(1, len(arr1)): # find samllest larger than prev
-            if not prevSorted:
-                break
-            if i > 0 and arr1[i - 1] >= arr1[i]:
-                prevSorted = False
-            ctr = 0
-
-            tmp = s2
-            for s in range(i, len(arr1)):
-                if arr1[s - 1] >= arr1[s]:
-                    if tmp == len(arr2) or arr2[tmp] >= arr1[s - 1]:
-                        break # no solution found here
-                    ctr += 1
-                while tmp < len(arr2) and arr2[tmp] <= arr1[s]:
-                    tmp += 1
-
-            if s == len(arr1):
-                minctr = min(minctr, ctr)
-
-            while s2 < len(arr2) and arr2[s2] <= arr1[i]:
-                s2 += 1
-
-        return -1 if float('inf') == minctr else minctr
+        d1ct = {-1: 0}
+        # after making the current index value increasing and end with k, v swaps are needed
+        for i in arr1:
+            tmp = collections.defaultdict(lambda: float('inf'))
+            for k, v in d1ct.items():
+                if k < i:
+                    # safely reuse it
+                    tmp[i] = min(v, tmp[i])
+                nxt = bisect.bisect_right(arr2, k)
+                if nxt < len(arr2):
+                    nxt = arr2[nxt]
+                    tmp[nxt] = min(1 + d1ct[k], tmp[nxt])
+            d1ct = tmp
+        return min(d1ct.values()) if d1ct else -1
